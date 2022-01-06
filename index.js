d4c6a0e82511683ee1a4bdf79f5198c049c67635
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2022-01-06 21:53:59 +0300

    Update index.js
    Translate error.

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 require("readline").createInterface({ input: process.stdin, output: process.stdout})
-
-.question(`Kullanıcı adını giriniz:   `, require("./getInfo.js"))
+.question(`Enter username:   `, require("./getInfo.js"))
